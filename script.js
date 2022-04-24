f3c325222ac0b37d192dc106251a2e9a5adc1987
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-04-24 15:11:03 -0400

    starting java

---
+++ script.js
@@ -0,0 +1,9 @@
+var cityInput = document.querySelector("city-input");
+var cityBtn = document.querySelector("search-btn");
+var cityNameE1 = document.querySelector("city-name");
+var historyClearBtn = document.querySelector("clear-btn");
+
+var cityArr = [];
+var api = "";
+
+
